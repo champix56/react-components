c29031f3db40c06b7f62f2c0b972bf13c61201ea
alex <desorbaix@free.fr>
2020-07-04 05:26:13 +0200

    base ts CRA + generate-react-cli

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './App.module.scss';
+const App: React.FC = () => (
+  <div className={styles.App} data-testid="App">
+    
+  </div>
+);
+
+export default App;
